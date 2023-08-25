2264cb456ba6fb735aade1764c1f6fb631a56a17
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-08-25 11:05:17 -0400

    added seed file and script to package.json

---
+++ seeds/seed.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+// MongoDB database
+mongoose.connect('socialsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Users
+const users = [
+  {
+    userName: 'user1',
+    email: 'user1@example.com',
+    thoughts: [],
+    friends: [], 
+  },
+  {
+    userName: 'user2',
+    email: 'user2@example.com',
+    thoughts: [],
+    friends: [],
+  },
+  {
+    userName: 'user3',
+    email: 'user3@example.com',
+    thoughts: [],
+    friends: [],
+  },
+  {
+    userName: 'user4',
+    email: 'user4@example.com',
+    thoughts: [],
+    friends: [],
+  },
+  {
+    userName: 'user5',
+    email: 'user5@example.com',
+    thoughts: [],
+    friends: [],
+  },
+];
+
+async function seed() {
+  try {
+    // Clear existing data
+    await User.deleteMany({});
+
+    // Create users
+    const createdUsers = await User.create(users);
+
+    console.log('Seed data created successfully.');
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
+    // Close DB connection
+    mongoose.connection.close();
+  }
+}
+
+seed();
